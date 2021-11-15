afc5e401f55a1379f9a6ca845120e7cf2c7e5e0e
Dingsen Shi <dingsen2@illinois.edu>
2021-11-15 13:04:12 -0600

    Two Column ListView of EditMem

---
+++ app/src/main/java/edu/illinois/cs465/pandemicpass/AddStatusActivity.java
@@ -0,0 +1,14 @@
+package edu.illinois.cs465.pandemicpass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddStatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_status);
+    }
+}
