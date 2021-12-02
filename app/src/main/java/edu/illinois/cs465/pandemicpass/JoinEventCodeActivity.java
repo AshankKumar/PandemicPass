037541abb0244d0b42e32b47bcbeae0c42179fdc
Tony Ruan <tonyr2@illinois.edu>
2021-12-02 11:19:15 -0600

    Prevent users from joining the same event twice

--- app/src/main/java/edu/illinois/cs465/pandemicpass/JoinEventCodeActivity.java
+++ app/src/main/java/edu/illinois/cs465/pandemicpass/JoinEventCodeActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,7 +26,11 @@ public class JoinEventCodeActivity extends AppCompatActivity implements View.OnC
 
     private Button next;
     private TextView eventCodeTextView;
+    private String userId;
     private DatabaseReference dbReferenceEvent;
+    private DatabaseReference dbReferenceUserWithUserIdEventAttendance;
+    private Event e;
+    private String eventId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +38,16 @@ public class JoinEventCodeActivity extends AppCompatActivity implements View.OnC
         setContentView(R.layout.activity_join_event_code);
 
         dbReferenceEvent = FirebaseDatabase.getInstance().getReference("Event");
+        userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        dbReferenceUserWithUserIdEventAttendance = FirebaseDatabase.getInstance().getReference("User").child(userId).child("eventAttendance");
+
         eventCodeTextView = (TextView) findViewById(R.id.eventCode);
 
         next = (Button) findViewById(R.id.next);
         next.setOnClickListener(this);
+
+        e = null;
+        eventId = "";
     }
 
     @Override
@@ -61,20 +72,45 @@ public class JoinEventCodeActivity extends AppCompatActivity implements View.OnC
                     Toast.makeText(JoinEventCodeActivity.this, "Invalid event code", Toast.LENGTH_LONG).show();
                 }
                 else {
+                    // can't join an event you're already attending
+                    // can still join event you're hosting (maybe because you want everyone included in event details)
+
+
+
                     Intent selectMembersIntent = new Intent(JoinEventCodeActivity.this, JoinSelectMembersActivity.class);
+
                     for(DataSnapshot snap: snapshot.getChildren()) {
-                        String eventId = snap.getKey();
-                        Event e = (Event) snap.getValue(Event.class);
-                        String eventName = e.eventName;
-                        String eventDate = e.date;
-                        selectMembersIntent.putExtra("eventId", eventId);
-                        selectMembersIntent.putExtra("eventName", eventName);
-                        selectMembersIntent.putExtra("eventDate", eventDate);
+                        eventId = snap.getKey();
+                        e = (Event) snap.getValue(Event.class);
                         break;
                     }
-                    selectMembersIntent.putExtra("eventCode", eventCode);
-                    startActivity(selectMembersIntent);
-                    JoinEventCodeActivity.this.finish();
+
+                    dbReferenceUserWithUserIdEventAttendance.orderByChild("eventId").equalTo(eventId).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if (!snapshot.exists()) {
+                                String eventName = e.eventName;
+                                String eventDate = e.date;
+                                selectMembersIntent.putExtra("eventId", eventId);
+                                selectMembersIntent.putExtra("eventName", eventName);
+                                selectMembersIntent.putExtra("eventDate", eventDate);
+                                selectMembersIntent.putExtra("eventCode", eventCode);
+                                startActivity(selectMembersIntent);
+                                JoinEventCodeActivity.this.finish();
+                            }
+                            else {
+                                Toast.makeText(JoinEventCodeActivity.this, "You've already joined this event.", Toast.LENGTH_LONG).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
+
                 }
             }
 
